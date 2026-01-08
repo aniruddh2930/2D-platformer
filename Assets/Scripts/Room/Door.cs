8a3bd99406df631ef22586ee209225e0722ed831
aniruddh2930 <ani4beal@gmail.com>
2026-01-08 20:30:02 +0000

    bug fixes and doors change

--- Assets/Scripts/Room/Door.cs
+++ Assets/Scripts/Room/Door.cs
@@ -3,22 +3,47 @@
 public class Door : MonoBehaviour
 {
     [SerializeField] private GameObject nextRoom;
-    [SerializeField] private GameObject lastRoom;
-    [SerializeField] private CameraMovement cam;
+    [SerializeField] private GameObject cam;
+    [Header("Enemies in current room")]
+    [SerializeField] private GameObject[] enemiesDead;
+    private int deadEnemies = 0;
 
+    private void Update()
+    {
+        foreach (GameObject enemy in enemiesDead)
+        {
+            if (enemy.activeInHierarchy == true)
+                return;
+            deadEnemies++;
+        }
+
+        if (deadEnemies == enemiesDead.Length)
+        {
+            StartCoroutine(DisableDoor());
+            this.enabled = false;
+        }
+    }
+    private System.Collections.IEnumerator DisableDoor()
+    {
+        yield return new WaitForSeconds(1f);
+        for (int i = 0; i < 10; i++)
+        {
+            if(i%2==0)
+                cam.transform.Translate(new Vector3(-1,0,0));
+            else
+                cam.transform.Translate(new Vector3(1,0,0));
+            transform.Translate(new Vector3(0,-0.5f, 0));
+            yield return new WaitForSeconds(0.1f);
+        }
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     { 
-        if (collision.tag == "Player" && collision.transform.position.x < transform.position.x)
+        if (collision.tag == "Player")
         {
             nextRoom.GetComponent<Reset>().ActivateRoom(true);
-            lastRoom.GetComponent<Reset>().ActivateRoom(false);
-            cam.MoveToNewRoom(nextRoom.transform);
-        }
-        else if (collision.tag == "Player" && collision.transform.position.x > transform.position.x)
-        {
-            nextRoom.GetComponent<Reset>().ActivateRoom(false);
-            lastRoom.GetComponent<Reset>().ActivateRoom(true);
-            cam.MoveToNewRoom(lastRoom.transform);
+            gameObject.SetActive(false);
         }
     }
 
--- Assets/Scripts/Room/Reset.cs
+++ Assets/Scripts/Room/Reset.cs
@@ -2,30 +2,31 @@ using UnityEngine;
 
 public class Reset : MonoBehaviour
 {
-    [SerializeField] private GameObject[] enemies;
+
+    [SerializeField] private GameObject[] enemiesAndDoor;
     private Vector2[] initialPositions;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (enemies.Length == 1 && enemies[0]==null)
+        if (enemiesAndDoor.Length == 0 )
             return;
-        initialPositions = new Vector2[enemies.Length];
-        for (int i = 0; i < enemies.Length; i++)
+        initialPositions = new Vector2[enemiesAndDoor.Length];
+        for (int i = 0; i < enemiesAndDoor.Length; i++)
         {
-            initialPositions[i] = enemies[i].transform.position;
+            initialPositions[i] = enemiesAndDoor[i].transform.position;
         }
     }
 
  
     public void ActivateRoom(bool status)
     {
-        if (enemies.Length == 1 && enemies[0] == null)
+        if (enemiesAndDoor.Length == 0)
             return;
-        for (int i= 0;i< enemies.Length; i++)
+        for (int i= 0;i< enemiesAndDoor.Length; i++)
         {
-            if (enemies[i] != null)
-                enemies[i].SetActive(status);
-                enemies[i].transform.position = initialPositions[i];
+            if (enemiesAndDoor[i] != null)
+                enemiesAndDoor[i].SetActive(status);
+                enemiesAndDoor[i].transform.position = initialPositions[i];
         }
     }
 }
