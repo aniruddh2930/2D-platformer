f354f1ca55808e2f43f80730b418c0f883673ad7
aniruddh2930 <ani4beal@gmail.com>
2026-02-16 22:02:49 +0000

    small fix health container

--- Assets/Scripts/Health/HealthContainer.cs
+++ Assets/Scripts/Health/HealthContainer.cs
@@ -8,12 +8,15 @@ public class HealthContainer : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Health health = collision.GetComponent<Health>();
-        if (collision.CompareTag("Player") && !(health.currentHealth == 3))
+        if (collision.CompareTag("Player"))
         {
-            health.AddHealth(regen);
-            AudioManager.instance.PlaySound(pickupSound);
-            gameObject.SetActive(false);
+            Health health = collision.GetComponent<Health>();
+            if (!(health.currentHealth == 3))
+            {
+                health.AddHealth(regen);
+                AudioManager.instance.PlaySound(pickupSound);
+                gameObject.SetActive(false);
+            }
         }
     }
     
